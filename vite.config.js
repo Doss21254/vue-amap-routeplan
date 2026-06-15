6fa5ade47d0ad731151b3f341eedc389e29f6ee8
Doss21254 <3200728513@qq.com>
2026-06-15 17:48:25 +0800

    优化vite配置打包

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
+    // @ 直接等价 src，导入组件不用写../../
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    port: 5173,
+    open: true, // 启动自动打开浏览器，提升开发效率
+    host: '0.0.0.0' // 允许局域网他人访问你的页面，团队调试方便
+  },
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000, // 调整分包体积告警阈值，优化打包体积
+    // 生产环境分包拆分，避免单js文件过大，页面加载更快
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ['vue'],
+        }
+      }
     }
   }
 })
